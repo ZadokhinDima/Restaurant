38cec3eb7f93c91af1545139a94130c361ee9adf
ZadokhinDima <dimazadohin@gmail.com>
2017-09-24 14:58:44 +0300

    Services

---
+++ src/main/java/model/service/LoginService.java
@@ -0,0 +1,14 @@
+package model.service;
+
+
+import model.entities.User;
+
+import java.util.Optional;
+
+public class LoginService {
+
+    public Optional<User> logIn(String email, String password){
+        return
+    }
+
+}
